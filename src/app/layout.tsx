87689a661fc4f9694993c55090884d95ba89077e
Christotoa <183560865+Chrisu-Angel@users.noreply.github.com>
2026-08-23 21:08:31 +0530

    layout: changed page title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Create ABCon Landing Page",
+  title: "ABCon Landing Page",
   description: "This is ABCon Landing page",
 };
 
